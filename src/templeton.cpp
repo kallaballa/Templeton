bf5c129b2f8f8c75b2ec885cbebcb35dc88ff100
kallaballa <amir@viel-zu.org>
2016-11-06 10:57:34 +0100

    cleanup

--- src/templeton.cpp
+++ src/templeton.cpp
@@ -112,32 +112,6 @@ Features detectFeatures(const string& filename) {
   return features;
 }
 
-typedef Vec6f Triangle_t;
-
-// Draw delaunay triangles
-static void draw_delaunay( Mat& img, Subdiv2D& subdiv, Scalar delaunay_color )
-{
-
-    vector<Vec6f> triangleList;
-    subdiv.getTriangleList(triangleList);
-    vector<Point> pt(3);
-    Size size = img.size();
-    Rect rect(0,0, size.width, size.height);
-
-    for( size_t i = 0; i < triangleList.size(); i++ ) {
-      Vec6f t = triangleList[i];
-      pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
-      pt[1] = Point(cvRound(t[2]), cvRound(t[3]));
-      pt[2] = Point(cvRound(t[4]), cvRound(t[5]));
-
-      if ( rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2])) {
-        line(img, pt[0], pt[1], delaunay_color, 1, CV_AA, 0);
-        line(img, pt[1], pt[2], delaunay_color, 1, CV_AA, 0);
-        line(img, pt[2], pt[0], delaunay_color, 1, CV_AA, 0);
-      }
-    }
-}
-
 void makeWhiteTransparent(Mat& m) {
   // find all white pixel and set alpha value to zero:
   for (int y = 0; y < m.rows; ++y)
